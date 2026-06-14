075288c74775ebd8673151274beb784e00e3ba3c
abbos <abbos5039abbos@gmail.com>
2026-06-14 10:38:51 +0500

    methoda exapmpels ishlash

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(password("1234"));
+        System.out.println(password("abbos50"));
+        System.out.println(password("abbos5039"));
+        System.out.println(password(""));
+
+    }
+    public static String password(String pass){
+
+        if (pass.isEmpty()){
+            return "empty";
+        }
+
+        int length = pass.length();
+        if (length >= 1 && length <= 5){
+            return "weak";
+        } else if (length >= 6 && length <= 8){
+            return "normal";
+        } else {
+            return "strong";
+        }
+
     }
 }
